d5ed988b5bd85ff6987a321fdf87beb6fc2ad362
Vitaly <onebeattrue@gmail.com>
2023-11-07 19:57:10 +0300

    Установка изображения

--- app/src/main/java/edu/mirea/onebeattrue/activityresultapicompose/MainActivity.kt
+++ app/src/main/java/edu/mirea/onebeattrue/activityresultapicompose/MainActivity.kt
@@ -1,15 +1,38 @@
 package edu.mirea.onebeattrue.activityresultapicompose
 
+import android.net.Uri
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CardElevation
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
 import edu.mirea.onebeattrue.activityresultapicompose.ui.theme.ActivityResultApiComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +45,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    ActivityResultTest()
                 }
             }
         }
@@ -30,17 +53,62 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun ActivityResultTest() {
+    var imageUri by remember {
+        mutableStateOf(Uri.EMPTY)
+    }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ActivityResultApiComposeTheme {
-        Greeting("Android")
+    val launcher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.GetContent(),
+        onResult = { uri ->
+            imageUri = uri
+        }
+    )
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(8.dp),
+        verticalArrangement = Arrangement.SpaceEvenly,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Card(
+            modifier = Modifier
+                .size(300.dp)
+                .padding(16.dp),
+            shape = RoundedCornerShape(8.dp),
+            elevation = CardDefaults.cardElevation(
+                defaultElevation = 16.dp
+            )
+        ) {
+            AsyncImage(
+                modifier = Modifier.fillMaxSize(),
+                model = imageUri,
+                contentDescription = null
+            )
+        }
+
+        Column {
+            Button(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                onClick = {
+                    launcher.launch("image/*")
+                },
+                shape = RoundedCornerShape(8.dp)
+            ) {
+                Text(text = "Set image")
+            }
+            Button(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                onClick = {
+                    imageUri = Uri.EMPTY
+                },
+                shape = RoundedCornerShape(8.dp)
+            ) {
+                Text(text = "Clear")
+            }
+        }
     }
 }
